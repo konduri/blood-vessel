4c6fd71c9271c7f87423f3f4faf387177ab5f657
konduri <kondurivamshi@gmail.com>
2014-04-01 19:14:54 -0400

    functioning match filter.see todo.txt

--- code_written/matched_filter/matched_filter.m
+++ code_written/matched_filter/matched_filter.m
@@ -1,27 +1,45 @@
+close all
+clc
+
 im =  imread('../flow.png');
 im =  rgb2gray(im);
 
-threshold = 45;   %chosen based on results needs improvement
-L     = 9;        %from paper http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=34715&tag=1
-sigma = 2;        
-mean_filter = fspecial('average', [5 5]); %to remove noise
+threshold   = 35;   %chosen based on results needs improvement
+L           = 8;        %from paper http://ieeexplore.ieee.org/xpls/abs_all.jsp?arnumber=34715&tag=1
+sigma       = 2.5:0.5:3.5;        
+mean_filter = fspecial('average', [4 4]); %to remove noise
 
-imshow(im);
+
+% imshow(im);
 im  = imfilter(im, mean_filter);
-imshow(im)
+% imshow(im)
 
-gaus_filter = fspecial('gaussian',[L L], sigma);
-gaus_filter = 10*(gaus_filter - mean(gaus_filter(:)));
+% gaus   = [0 4 3 2 1 -2 -5 -6 -5 -2 1 2 3 4 0]/110;
 
-size_img   = size(im);
-no_filters = 180/15; %divide into sets of 15 degrees   
+size_img    = size(im);
+no_filters  = 180/15; %divide into sets of 15 degrees   
 
-filtered   = zeros([size_img no_filters]); %filter and store result
-for i = 1:no_filters
-    filtered(:,:,i) = imfilter(im,imrotate(gaus_filter,(i-1)*15));
+filtered    = zeros([size_img no_filters]); %filter and store result
+
+
+ 
+for k = 1:length(sigma)
+    gaus  = fspecial('gaussian',[1 16], sigma(k));
+    gaus = -gaus + mean(gaus);
+    gaus  = repmat(gaus,9,1);
+    gaus_filter = gaus;
+
+
+    for i = 1:no_filters
+        filtered(:,:,(k-1)*no_filters + i) = imfilter(im,imrotate(gaus_filter,(i-1)*15));
+    end
+
+
 end
-   
-vessel = zeros(size_img); 
+
+
+% ans = []
+vessel = zeros(size_img);
 
 for i = 1:size_img(1)
     for j =  1:size_img(2)
@@ -32,5 +50,19 @@ for i = 1:size_img(1)
     end
 end
 
-figure
-imshow(vessel)
+
+
+
+
+subplot(1,2,1)
+imshow(im);
+subplot(1,2,2)
+imshow(vessel);
+% imshow(vessel)
+%%
+%  vessel = vessel;
+
+
+%  imshow(vessel)
+ BW2 = bwareaopen(vessel,100);
+ imshow(BW2)
